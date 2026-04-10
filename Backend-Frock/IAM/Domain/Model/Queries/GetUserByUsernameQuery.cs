bd67ddf699c1ae03f451b1991da814a6b4797d3f
Néstor Velarde Gonzales <u20211c221@upc.edu.pe>
2026-04-09 21:15:59 -0500

    feat(iam): implement core domain model for IAM bounded context

---
+++ Backend-Frock/IAM/Domain/Model/Queries/GetUserByUsernameQuery.cs
@@ -0,0 +1,3 @@
+﻿namespace Backend_Frock.IAM.Domain.Model.Queries;
+
+public record GetUserByUsernameQuery(string Username);
---
+++ Backend-Frock/IAM/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Backend_Frock.IAM.Domain.Model.Aggregates;
+using Backend_Frock.Shared.Domain.Repositories;
+
+namespace Backend_Frock.IAM.Domain.Repositories;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+    /**
+     * <summary>
+     *     Find a user by id
+     * </summary>
+     * <param name="username">The username to search</param>
+     * <returns>The user</returns>
+     */
+    Task<User?> FindByUsernameAsync(string username);
+
+    Task<User?> FindByEmailAsync(string email);
+
+    Task<bool> ExistsByEmail(string email);
+}
