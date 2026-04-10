2e29152eedf21bacd12a288721a238c97d7981c8
Néstor Velarde Gonzales <u20211c221@upc.edu.pe>
2026-04-09 21:23:23 -0500

    feat(iam): implement application layer and use cases

---
+++ Backend-Frock/IAM/Application/QueryServices/UserQueryService.cs
@@ -0,0 +1,50 @@
+﻿using Backend_Frock.IAM.Domain.Model.Aggregates;
+using Backend_Frock.IAM.Domain.Model.Queries;
+using Backend_Frock.IAM.Domain.Repositories;
+using Backend_Frock.IAM.Domain.Services;
+
+namespace Backend_Frock.IAM.Application.QueryServices;
+
+public class UserQueryService(IUserRepository userRepository) : IUserQueryService
+{
+    /**
+     * <summary>
+     *     Handle get user by id query
+     * </summary>
+     * <param name="query">The query object containing the user id to search</param>
+     * <returns>The user</returns>
+     */
+    public async Task<User?> Handle(GetUserByIdQuery query)
+    {
+        return await userRepository.FindByIdAsync(query.Id);
+    }
+
+    /**
+     * <summary>
+     *     Handle get user by username query
+     * </summary>
+     * <param name="query">The query object for getting all users</param>
+     * <returns>The user</returns>
+     */
+    public async Task<IEnumerable<User>> Handle(GetAllUsersQuery query)
+    {
+        return await userRepository.ListAsync();
+    }
+
+    /**
+     * <summary>
+     *     Handle get user by username query
+     * </summary>
+     * <param name="query">The query object containing the username to search</param>
+     * <returns>The user</returns>
+     */
+    public async Task<User?> Handle(GetUserByUsernameQuery query)
+    {
+        return await userRepository.FindByUsernameAsync(query.Username);
+    }
+
+    public async Task<User?> Handle(GetUserByEmailQuery query)
+    {
+        return await userRepository.FindByEmailAsync(query.Email);
+    }
+}
