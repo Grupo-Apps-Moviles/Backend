c00542ddfe3b33dfe2c6d5874b694c25356d4c28
Néstor Velarde Gonzales <u20211c221@upc.edu.pe>
2026-04-09 19:01:49 -0500

    feat(shared): implement base repository and unit of work patterns

---
+++ Backend-Frock/Shared/Application/Internal/EventHandlers/IEventHandler.cs
@@ -0,0 +1,8 @@
+﻿using Cortex.Mediator.Notifications;
+
+namespace Backend_Frock.Shared.Application.Internal.EventHandlers;
+
+public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
+{
+    
+}
