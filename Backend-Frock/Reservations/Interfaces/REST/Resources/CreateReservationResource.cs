49839e2970a0fd35c8c0f2beb34f358fdabfd09f
Adrian QC <aqc1019@gmail.com>
2026-06-16 23:47:37 -0500

    feat(reservation): add CreateReservationResource

--- Backend-Frock/Reservations/Interfaces/REST/Resources/CreateReservationResource.cs
+++ Backend-Frock/Reservations/Interfaces/REST/Resources/CreateReservationResource.cs
@@ -1,3 +1,9 @@
 ﻿namespace Backend_Frock.Reservations.Interfaces.REST.Resources;
 
-public record CreateReservationResource(int UserId, List<int> RouteIds);
+public record CreateReservationResource(
+    int UserId,
+    List<int> RouteIds,
+    decimal Amount,
+    string PaypalTransactionId,
+    int DriverId
+);
