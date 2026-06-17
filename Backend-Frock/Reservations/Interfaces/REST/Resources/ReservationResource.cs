dbf0fbb65296c31afbd0cada6040ab9525b21e2c
Adrian QC <aqc1019@gmail.com>
2026-06-16 23:48:21 -0500

    feat(reservation): add ReservationResource DTO

--- Backend-Frock/Reservations/Interfaces/REST/Resources/ReservationResource.cs
+++ Backend-Frock/Reservations/Interfaces/REST/Resources/ReservationResource.cs
@@ -1,3 +1,13 @@
 ﻿namespace Backend_Frock.Reservations.Interfaces.REST.Resources;
 
-public record ReservationResource(int Id, int UserId, string Status, List<int> RouteIds);
+public record ReservationResource(
+    int Id,
+    int UserId,
+    int DriverId,
+    string Status,
+    decimal Amount,
+    decimal DriverEarnings,
+    decimal PlatformFee,
+    string PaypalTransactionId,
+    DateTime CreatedAt
+);
