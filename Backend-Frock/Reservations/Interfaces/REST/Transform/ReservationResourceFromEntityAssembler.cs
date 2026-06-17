83c2788dc284984f1862c9a0bf0b8705f433f79b
Adrian QC <aqc1019@gmail.com>
2026-06-16 23:48:27 -0500

    feat(reservation): add ReservationResourceFromEntityAssembler

--- Backend-Frock/Reservations/Interfaces/REST/Transform/ReservationResourceFromEntityAssembler.cs
+++ Backend-Frock/Reservations/Interfaces/REST/Transform/ReservationResourceFromEntityAssembler.cs
@@ -3,11 +3,23 @@ using Backend_Frock.Reservations.Interfaces.REST.Resources;
 
 namespace Backend_Frock.Reservations.Interfaces.REST.Transform;
 
-public static class ReservationResourceFromEntityAssembler
+public static class
+    ReservationResourceFromEntityAssembler
 {
-    public static ReservationResource ToResourceFromEntity(Reservation entity)
+    public static ReservationResource
+        ToResourceFromEntity(
+            Reservation entity)
     {
-        var routeIds = entity.ReservationRoutes.Select(rr => rr.RouteId).ToList();
-        return new ReservationResource(entity.Id, entity.UserId, entity.Status.ToString(), routeIds);
+        return new ReservationResource(
+            entity.Id,
+            entity.UserId,
+            entity.DriverId,
+            entity.Status.ToString(),
+            entity.Amount,
+            entity.DriverEarnings,
+            entity.PlatformFee,
+            entity.PaypalTransactionId,
+            entity.CreatedAt
+        );
     }
 }
